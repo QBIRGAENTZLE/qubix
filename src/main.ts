ed3d377e143059441cfb8c3bff4586fd2f66b6c1
Quentin <qbi@outlook.fr>
2019-04-10 17:49:17 +0200

    GA Test

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
     enableProdMode();
-    Angulartics2GoogleAnalytics.prototype.setUserProperties({
-        domain: 'auto',
-        trackingId: 'UA-138035847-1'
-    });
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
