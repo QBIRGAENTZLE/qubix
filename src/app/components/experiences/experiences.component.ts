e111979647b16eebc2f99d5b7cc98008f1a6da4e
Quentin <qbi@outlook.fr>
2019-07-16 13:46:01 +0200

    WIP Responsive
    Experience and formation

--- src/app/components/experiences/experiences.component.ts
+++ src/app/components/experiences/experiences.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { ExperiencesService } from '@providers/experiences.service';
+import { ResponsiveService } from '@providers/responsive.service';
 
 import { Experience } from '@models/experience';
 
@@ -9,13 +11,33 @@ import { Experience } from '@models/experience';
     templateUrl: './experiences.component.html',
     styleUrls: ['./experiences.component.scss']
 })
-export class ExperiencesComponent {
+export class ExperiencesComponent implements OnDestroy {
 
     public experiencesList: Experience[];
 
+    private responsiveSizeSubscription: Subscription;
+    public isMobileSize: boolean;
+
     constructor(
         private experiencesService: ExperiencesService,
+        private responsiveService: ResponsiveService
     ) {
         this.experiencesList = this.experiencesService.getExperiencesList();
+
+        if (this.responsiveService.isMobileSize()) {
+            this.isMobileSize = true;
+        }
+
+        this.responsiveSizeSubscription = this.responsiveService.obsResponsiveSize().subscribe(size => {
+            if (size === 'xs' || size === 'sm') {
+                this.isMobileSize = true;
+            } else {
+                this.isMobileSize = false;
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.responsiveSizeSubscription.unsubscribe();
     }
 }
--- src/app/components/formation/formation.component.ts
+++ src/app/components/formation/formation.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { FormationService } from '@providers/formation.service';
+import { ResponsiveService } from '@providers/responsive.service';
 
 import { Formation } from '@models/formation';
 
@@ -9,14 +11,34 @@ import { Formation } from '@models/formation';
     templateUrl: './formation.component.html',
     styleUrls: ['./formation.component.scss']
 })
-export class FormationComponent {
+export class FormationComponent implements OnDestroy {
 
     public formationList: Formation[];
 
+    private responsiveSizeSubscription: Subscription;
+    public isMobileSize: boolean;
+
     constructor(
-        private formationService: FormationService
+        private formationService: FormationService,
+        private responsiveService: ResponsiveService
     ) {
         this.formationList = this.formationService.getFormationList();
+
+        if (this.responsiveService.isMobileSize()) {
+            this.isMobileSize = true;
+        }
+
+        this.responsiveSizeSubscription = this.responsiveService.obsResponsiveSize().subscribe(size => {
+            if (size === 'xs' || size === 'sm') {
+                this.isMobileSize = true;
+            } else {
+                this.isMobileSize = false;
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.responsiveSizeSubscription.unsubscribe();
     }
 
 }
